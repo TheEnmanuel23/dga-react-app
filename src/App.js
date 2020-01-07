34ddc430c4815ee9faaba37f273e898a0f241c3e
Enmanuel jarquin <enmisac@gmail.com>
2020-01-06 22:17:37 -0600

    rutas basicas del layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-a">Page A</Link>
+          </li>
+          <li>
+            <Link to="/page-b">Page B</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="layout-content">
+        <Switch>
+          <Route exact path="/" component={() => <p>Home</p>} />
+          <Route path="/page-a" component={() => <p>Page A</p>} />
+          <Route path="/page-b" component={() => <p>Page B</p>} />
+        </Switch>
+      </div>
     </div>
   );
 }
